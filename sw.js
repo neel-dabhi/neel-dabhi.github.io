d0d6ef3b9eb43e4b083d201db228585cb6c8a8ed
neelkanth <nl543396@dal.ca>
2020-08-19 16:59:15 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-663f3367739a0a95e86c.js"
+    "url": "webpack-runtime-7f5f310f0cbacb357305.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1bdf6c79ed27d072403ac8ccfe25dfc2"
+    "revision": "7e4eef68a968d817f04e7b3d110a389e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f1919e302f55866e7e1aa027b97623eb"
+    "revision": "27819c2d822c345b14e5ce767169eed2"
   },
   {
     "url": "manifest.webmanifest",
